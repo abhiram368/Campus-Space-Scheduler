9bb53942230214723722e868c09d031a9d835621
Venkat Bayanaboina <195527986+venkatbayanaboina@users.noreply.github.com>
2026-03-15 14:37:43 +0530

    feat: add ViewDocumentActivity

---
+++ app/src/main/java/com/example/hod/hod/ViewDocumentActivity.java
@@ -0,0 +1,16 @@
+package com.example.hod.hod;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.hod.R;
+
+public class ViewDocumentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_document);
+    }
+}
